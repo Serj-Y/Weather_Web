1cb2c0adec68c1b48f94f977c5e52c2f3b1f6394
Serj-Y <newlife101101@gmail.com>
2023-08-31 15:26:20 +0300

    add top buttons component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-
+import TopButtons from './components/TopButtons';
 
 
 function App() {
   return (
-    <div className="flex  w-screen h-screen  justify-center items-center bg-slate-300 align ">
-   
-      <h1 className= " text-9xl  text-blue-500  "  >
-      Weather App
-    </h1>
+    
+      <div className="flex mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-500 to-blue-500 h-fit shadow-xl shadow-gray-400">
+<TopButtons/>
 
-    </div>
+
+      </div>
+
+
 
   );
 }
---
+++ src/components/TopButtons.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export default function TopButtons() {
+
+  const cites = [
+    {
+      id: 1,
+      title: "Kyiv"
+    },
+    {
+      id: 2,
+      title: "London"
+    },
+    {
+      id: 3,
+      title: "Milan"
+    },
+    {
+      id: 4,
+      title: "Washington"
+    }
+  ]
+
+  return (
+<div className=" flex justify-around w-full items-center my-6">
+  {cites.map((city) => (
+    <button key={city.id} className="text-white text-lg font-medium">{city.title}</button>
+  ))}
+</div>
+  )
+}
